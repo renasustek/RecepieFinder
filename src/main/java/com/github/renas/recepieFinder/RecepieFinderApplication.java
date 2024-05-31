e2ef52a1d498e6bbc89b0cb303ab641973c94fd3
renasustek <renasustek@gmail.com>
2024-05-31 20:08:33 +0100

    First Commit

---
+++ src/main/java/com/github/renas/recepieFinder/RecepieFinderApplication.java
@@ -0,0 +1,13 @@
+package com.github.renas.recepieFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepieFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepieFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/renas/recepieFinder/RecepieFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.renas.recepieFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepieFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
