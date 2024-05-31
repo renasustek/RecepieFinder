51cdad6c534744f416da074d55f41184619bd655
renasustek <renasustek@gmail.com>
2024-05-31 21:04:25 +0100

    Docker mysql

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,6 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
---
+++ docker/db/sql/schema.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `recepie_finder` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `recepie_finder`;
