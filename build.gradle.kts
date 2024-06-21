3fd1b2d59f57dab2a148bde097d04ea0ef9d4fb6
renasustek <renasustek@gmail.com>
2024-06-21 22:58:46 +0100

    fixed errors sonar cube very rudley made appaerent to e

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
 	java
+	jacoco
+	`jacoco-report-aggregation`
+	id("com.diffplug.spotless") version "7.0.0.BETA1"
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
 	id("org.sonarqube") version "4.4.1.3373"
@@ -9,21 +12,14 @@ group = "com.github.renas"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
 }
 
 repositories {
 	mavenCentral()
 }
-
-sonar {
-	properties {
-		property("sonar.projectKey", "renasustek_RecepieFinder")
-		property("sonar.organization", "renasustek")
-		property("sonar.host.url", "https://sonarcloud.io")
-	}
-}
-
 
 dependencies {
 
@@ -41,6 +37,43 @@ dependencies {
 	testImplementation("io.projectreactor:reactor-test")
 }
 
-tasks.withType<Test> {
+tasks.test {
 	useJUnitPlatform()
 }
+
+tasks.testCodeCoverageReport {
+	dependsOn(tasks.test)
+	executionData(fileTree(layout.buildDirectory).include("jacoco/*.exec"))
+	reports {
+		xml.required = true
+		html.required = true
+	}
+	mustRunAfter(tasks.spotlessApply)
+}
+
+tasks.check {
+	dependsOn(tasks.testCodeCoverageReport)
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "renasustek_RecepieFinder")
+		property("sonar.organization", "renasustek")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
+}
+tasks.sonar {
+	dependsOn(tasks.check)
+}
+
+spotless {
+	ratchetFrom("origin/main")
+	java {
+		toggleOffOn()
+		palantirJavaFormat("2.47.0").formatJavadoc(true)
+		removeUnusedImports()
+		trimTrailingWhitespace()
+		endWithNewline()
+	}
+}
+
