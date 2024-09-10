b41da01e0648beba55fadca6066e9386b0c2eb84
renasustek <renasustek@gmail.com>
2024-09-10 22:35:52 +0100

    ,ore tests

--- src/test/java/com/github/renas/recipe/jackson/QuantityJacksonModuleTest.java
+++ src/test/java/com/github/renas/recipe/jackson/QuantityJacksonModuleTest.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.github.renas.recipe.measurment.Mass;
 import com.github.renas.recipe.measurment.Quantity;
 import com.github.renas.recipe.measurment.Unit;
+import com.github.renas.recipe.measurment.Volume;
 import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,9 +40,33 @@ class QuantityJacksonModuleTest {
     }
 
     @Test
-    void serializeQuantitySuccessfully() throws IOException {
+    void serializeMassKilogramSuccessfully() throws IOException {
         Quantity quantity = new Mass(5, Unit.KILOGRAM);
         String expectedJson = "{\"quantity\":5.0,\"unit\":\"KILOGRAM\"}";
+        String jsonResult = objectMapper.writeValueAsString(quantity);
+        assertEquals(expectedJson, jsonResult);
+    }
+
+    @Test
+    void serializeMassgramSuccessfully() throws IOException {
+        Quantity quantity = new Mass(5, Unit.GRAM);
+        String expectedJson = "{\"quantity\":5.0,\"unit\":\"GRAM\"}";
+        String jsonResult = objectMapper.writeValueAsString(quantity);
+        assertEquals(expectedJson, jsonResult);
+    }
+
+    @Test
+    void serializeVolumeMlSuccessfully() throws IOException {
+        Quantity quantity = new Volume(5, Unit.MILLILITER);
+        String expectedJson = "{\"quantity\":5.0,\"unit\":\"MILLILITER\"}";
+        String jsonResult = objectMapper.writeValueAsString(quantity);
+        assertEquals(expectedJson, jsonResult);
+    }
+
+    @Test
+    void serializeVolumeTspSuccessfully() throws IOException {
+        Quantity quantity = new Volume(5, Unit.TEASPOON);
+        String expectedJson = "{\"quantity\":5.0,\"unit\":\"TEASPOON\"}";
         String jsonResult = objectMapper.writeValueAsString(quantity);
         assertEquals(expectedJson, jsonResult);
     }
