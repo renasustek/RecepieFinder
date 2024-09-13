fc96e3b0178c8801f50e4a454108a81b27ab1cfc
renasustek <renasustek@gmail.com>
2024-09-14 00:53:35 +0100

    more tests, remember the difference between injectMocks and Mock next time ;)

--- src/test/java/com/github/renas/recipe/RecepieFinderApplicationTest.java
+++ src/test/java/com/github/renas/recipe/RecepieFinderApplicationTest.java
@@ -1,13 +1,10 @@
 package com.github.renas.recipe;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class RecepieFinderApplicationTest {
     @Test
     void main() {
         RecepieFinderApplication.main(new String[] {});
-        assertTrue(true);
     }
 }
