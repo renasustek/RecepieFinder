cebb4573c9e67f5305cbae31108bc487d91fa882
renasustek <renasustek@gmail.com>
2024-10-29 00:02:06 +0000

    made sonar lint changes

--- src/main/java/com/github/renas/recipe/service/StructureIngredients.java
+++ src/main/java/com/github/renas/recipe/service/StructureIngredients.java
@@ -3,9 +3,11 @@ package com.github.renas.recipe.service;
 import static com.github.renas.recipe.measurment.Unit.*;
 
 import com.github.renas.recipe.measurment.Mass;
+import com.github.renas.recipe.measurment.NoUnit;
 import com.github.renas.recipe.measurment.Quantity;
 import com.github.renas.recipe.measurment.Volume;
 import com.github.renas.recipe.request.ingredient.Ingredient;
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Component;
@@ -13,7 +15,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class StructureIngredients {
 
-    public static <T extends Quantity> Ingredient<T> stringToQuantity(String ingredient) {
+    public static Ingredient<Quantity> stringToQuantity(String ingredient) {
         ArrayList<String> units = new ArrayList<>(List.of(
                 "g", "gram", "kg", "kilogram", "ml", "millilitre", "litre", "tablespoon", "tbsp", "teaspoon", "tsp"));
 
@@ -28,31 +30,68 @@ public class StructureIngredients {
             }
         }
 
-        if (unitFound == null) {
-            return null;
-        }
-
         double amount;
-        try {
-            amount = Double.parseDouble(ingredient.substring(0, unitIndex).trim());
-        } catch (NumberFormatException e) {
-            return null;
+        String name;
+
+        if (unitFound != null) {
+            String amountStr = ingredient.substring(0, unitIndex).trim();
+            try {
+                amount = amountStringToDouble(amountStr);
+            } catch (NumberFormatException e) {
+                return null;
+            }
+            name = ingredient.substring(unitIndex + unitFound.length()).trim();
+        } else {
+            Ingredient<NoUnit> noUnit = noUnitFound(ingredient);
+            return new Ingredient<>(noUnit.getQuantity(), noUnit.getName());
         }
-        String name = ingredient.substring(unitIndex + unitFound.length()).trim();
 
         return new Ingredient<>(parse(unitFound, amount), name);
     }
 
+    private static Ingredient<NoUnit> noUnitFound(String ingredient) {
+        int index = -1;
+
+        for (int i = 0; i < ingredient.length(); i++) {
+            if (!Character.isDigit(ingredient.charAt(i))) {
+                index = i;
+                break;
+            }
+        }
+        if (index == 0) {
+            return new Ingredient<>(new NoUnit(1), ingredient);
+        }
+
+        String amount = ingredient.substring(0, index).trim();
+        String name = ingredient.substring(index).trim();
+        // todo assumes the number is always first
+        return new Ingredient<>(new NoUnit(Double.parseDouble(amount)), name);
+    }
+
     @SuppressWarnings("unchecked")
-    private static <T extends Quantity> T parse(String unit, double value) {
+    private static Quantity parse(String unit, double value) {
         return switch (unit) {
-            case "g", "gram", "grams" -> (T) new Mass(value, GRAM);
-            case "kg", "kilogram", "kilograms" -> (T) new Mass(value, KILOGRAM);
-            case "ml", "millilitre", "millilitres" -> (T) new Volume(value, MILLILITER);
-            case "l", "litre", "litres" -> (T) new Volume(value, LITER);
-            case "tbsp", "tablespoon", "tablespoons" -> (T) new Volume(value, TABLESPOON);
-            case "tsp", "teaspoon", "teaspoons" -> (T) new Volume(value, TEASPOON);
+            case "g", "gram", "grams" -> new Mass(value, GRAM);
+            case "kg", "kilogram", "kilograms" -> new Mass(value, KILOGRAM);
+            case "ml", "millilitre", "millilitres" -> new Volume(value, MILLILITER);
+            case "l", "litre", "litres" -> new Volume(value, LITER);
+            case "tbsp", "tablespoon", "tablespoons" -> new Volume(value, TABLESPOON);
+            case "tsp", "teaspoon", "teaspoons" -> new Volume(value, TEASPOON);
             default -> null;
         };
+    }
+
+    private static double parseFractions(String amount) throws NumberFormatException {
+        String[] fraction = Normalizer.normalize(amount, Normalizer.Form.NFKD).split("\u2044");
+        return (double) Integer.parseInt(fraction[0]) / Integer.parseInt(fraction[1]);
+    }
+
+    private static double amountStringToDouble(String amount) throws NumberFormatException {
+        String numericRegex = "-?\\d+(\\.\\d+)?";
+        if (amount.matches(numericRegex)) {
+            return Double.parseDouble(amount);
+        } else {
+            return parseFractions(amount);
+        }
     }
 }
--- src/test/java/com/github/renas/recipe/service/StructureIngredientsTest.java
+++ src/test/java/com/github/renas/recipe/service/StructureIngredientsTest.java
@@ -2,12 +2,10 @@ package com.github.renas.recipe.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import com.github.renas.recipe.measurment.Mass;
-import com.github.renas.recipe.measurment.Quantity;
-import com.github.renas.recipe.measurment.Unit;
-import com.github.renas.recipe.measurment.Volume;
+import com.github.renas.recipe.measurment.*;
 import com.github.renas.recipe.request.ingredient.Ingredient;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -39,5 +37,36 @@ class StructureIngredientsTest {
                         "250g pouch ready-to-eat quinoa (we used Merchant Gourmet)",
                         new Ingredient<>(
                                 new Mass(250, Unit.GRAM), "pouch ready-to-eat quinoa (we used Merchant Gourmet)")));
+    }
+
+    @Test
+    void whenGivenNoUnitShouldReturnValidNoUnit() {
+        Ingredient<Quantity> actual = StructureIngredients.stringToQuantity("4 radishes, finely sliced");
+        Ingredient<Quantity> expected = new Ingredient<>(new NoUnit(4), "radishes, finely sliced");
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+        assertThat(actual.getQuantity().unit()).isEqualTo(expected.getQuantity().unit());
+        assertThat(actual.getQuantity().value())
+                .isEqualTo(expected.getQuantity().value());
+    }
+
+    @Test
+    void whenGivenNoUnitWithNoStartingDigitShouldReturnNoUnitWithValueOne() {
+        Ingredient<Quantity> actual = StructureIngredients.stringToQuantity("radishes, finely sliced");
+        Ingredient<Quantity> expected = new Ingredient<>(new NoUnit(1), "radishes, finely sliced");
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+        assertThat(actual.getQuantity().unit()).isEqualTo(expected.getQuantity().unit());
+        assertThat(actual.getQuantity().value())
+                .isEqualTo(expected.getQuantity().value());
+    }
+
+    @Test
+    void whenGivenSpecialCharacterAsAmountShouldReturnAsDouble() {
+        Ingredient<Quantity> actual = StructureIngredients.stringToQuantity("½ pack dill, finely chopped");
+        Ingredient<Quantity> expected =
+                new Ingredient<>(new Volume(0.5, Unit.TEASPOON), "harissa paste  or chilli powder");
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+        assertThat(actual.getQuantity().unit()).isEqualTo(expected.getQuantity().unit());
+        assertThat(actual.getQuantity().value())
+                .isEqualTo(expected.getQuantity().value());
     }
 }
